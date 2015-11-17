428d8ae97497000892b9a4c0bf043daa4486dd9c
Groupystinks <jason0911520@gmail.com>
2015-11-18 00:30:04 +0800

    added countdownTimer with deadline, time format and size as properties

--- src/App.js
+++ src/App.js
@@ -4,13 +4,13 @@ import CountdownTimer from './CountdownTimer';
 export default class App extends Component {
   constructor() {
     super();
-    // this.deadline = 'Oct 29 2020 00:00:50 UTC+0800';
-    // // year-day-hour-minute-second
-    // this.options = {format: 'year-day'};
   }
   render() {
     return (
-      <CountdownTimer deadline={'Oct 29 2020 00:00:50 UTC+0800'} options={{format: 'year-day-hour-minute-second'}} size="200px"/>
+      <CountdownTimer
+        deadline={'Oct 29 2020 00:00:50 UTC+0800'}
+        options={{format: 'year-day-hour-minute-second'}}
+        size="200px" />
     );
   }
 }
--- src/CountdownTimer.js
+++ src/CountdownTimer.js
@@ -1,25 +1,48 @@
 import React, {Component, PropTypes} from 'react';
 import ReactDOM from 'react-dom';
 
+
 class CountdownTimer extends Component {
   static displayName = 'CountdownTimer';
+
   static propTypes = {
     deadline: PropTypes.string.isRequired,
     options: PropTypes.object.isRequired,
     size: PropTypes.string,
   }
+
   constructor() {
     super();
-    this._radius = null;
-    this._fraction = null;
-    this._secondCanvasContext = null;
-    this.state = {
-      years: null,
-      days: null,
-      hours: null,
-      minutes: null,
-      seconds: null,
+    this._format;
+    this._parsedFormatArr;
+    this._radius;
+    this._fraction;
+    this._secondCanvasContext;
+    this._minuteCanvasContext;
+    this._hourCanvasContext;
+    this._dayCanvasContext;
+    this._yearCanvasContext;
+    this._timeMeasure = {
+      second: 60,
+      minute: 60,
+      hour: 24,
+      day: 365,
+      year: 1,
     };
+    this.state = {
+      year: '',
+      day: '',
+      hour: '',
+      minute: '',
+      second: '',
+    };
+  }
+
+  componentWillMount() {
+    this._radius = parseInt(this.props.size, 10) / 2;
+    this._format = this.props.options.format;
+    const parsedFormat = this._parseFormat(this._format);
+    this._parsedFormatArr = Object.keys(parsedFormat).filter(key => parsedFormat[key]);
   }
 
   componentDidMount() {
@@ -27,6 +50,40 @@ class CountdownTimer extends Component {
     this._drawBackground();
     this.initializeClock(this.props.deadline, this.props.options);
     this.setState({deadline: this.props.deadline});
+  }
+
+  initializeClock = (endtime, options) => {
+    const format = options.format.split('-').map((form) => {
+      return {[form]: form};
+    }).reduce((previousValue, currentValue) => {
+      return Object.assign(
+        {},
+        previousValue,
+        currentValue
+      );
+    });
+    this._updateClock(endtime, format);
+  }
+
+  _updateClock() {
+    const time = this._getTime(this.state.deadline);
+
+    if (time.total <= 0) {
+      clearTimeout(timeinterval);
+    }
+
+    this.setState({
+      year: time.years,
+      day: time.days,
+      hour: ('0' + time.hours).slice(-2),
+      minute: ('0' + time.minutes).slice(-2),
+      second: ('0' + time.seconds).slice(-2),
+    });
+
+    this._clearTimer();
+    this._drawTimer();
+
+    const timeinterval = setTimeout(this._updateClock.bind(this), 1000);
   }
 
   _getTime(endtime) {
@@ -45,8 +102,9 @@ class CountdownTimer extends Component {
       seconds,
     };
   }
-  initializeClock(endtime, options) {
-    const format = options.format.split('-').map((form) => {
+
+  _parseFormat = (format) => {
+    const parsedFormat = format.split('-').map((form) => {
       return {[form]: form};
     }).reduce((previousValue, currentValue) => {
       return Object.assign(
@@ -56,93 +114,108 @@ class CountdownTimer extends Component {
       );
     });
 
-    this._updateClock(endtime, format);
+    return parsedFormat;
   }
+
   _setupCanvas = () => {
-    const secondCanvas  = ReactDOM.findDOMNode(this.refs.second_canvas);
-    this._secondCanvasContext = secondCanvas.getContext('2d');
-    this._secondCanvasContext.textAlign = 'center';
-    this._secondCanvasContext.textBaseline = 'middle';
-    this._secondCanvasContext.font = 'bold 20px Arial';
-  }
-
-  _updateClock(endtime, format) {
-    const time = this._getTime(this.state.deadline);
-
-    if (time.total <= 0) {
-      clearTimeout(timeinterval);
-    }
-
-    this.setState({
-      years: time.years,
-      days: time.days,
-      hours: ('0' + time.hours).slice(-2),
-      minutes: ('0' + time.minutes).slice(-2),
-      seconds: ('0' + time.seconds).slice(-2),
+    this._parsedFormatArr.map(format => {
+      const propsName = '_' + format + 'CanvasContext';
+      const refsName = format + '_canvas';
+      const canvas  = ReactDOM.findDOMNode(this.refs[refsName]);
+      this[propsName] = canvas.getContext('2d');
+      this[propsName].textAlign = 'center';
+      this[propsName].textBaseline = 'middle';
+      this[propsName].font = 'bold 20px Arial';
     });
-    // this._updateClock();
-    this._clearTimer();
-    this._drawTimer();
-
-    const timeinterval = setTimeout(this._updateClock.bind(this), 1000);
   }
 
   _drawBackground = () => {
-    this._secondCanvasContext.beginPath();
-    this._secondCanvasContext.globalAlpha = 1 / 3;
-    this._secondCanvasContext.arc(80.64, 80.64, 80.64, 0, Math.PI * 2, false);
-    this._secondCanvasContext.arc(80.64, 80.64, 80.64 / 1.3, Math.PI * 2, 0, true);
-    this._secondCanvasContext.fill();
+    this._parsedFormatArr.map(format => {
+      const propsName = '_' + format + 'CanvasContext';
+      this[propsName].beginPath();
+      this[propsName].globalAlpha = 1 / 3;
+      this[propsName].arc(this._radius, this._radius, this._radius, 0, Math.PI * 2, false);
+      this[propsName].arc(this._radius, this._radius, this._radius / 1.3, Math.PI * 2, 0, true);
+      this[propsName].fill();
+    });
   }
 
   _clearTimer = () => {
-    this._secondCanvasContext.clearRect(0, 0, 1240, 1240);
+    this._parsedFormatArr.map(format => {
+      const propsName = '_' + format + 'CanvasContext';
+      this[propsName].clearRect(0, 0, 1240, 1240);
+    });
     this._drawBackground();
   }
 
   _drawTimer = () => {
-    const percent = 2 * parseInt(this.state.seconds, 10) / 60 - 4.5;
-    console.log(this.state.seconds);
-    this._secondCanvasContext.globalAlpha = 1 / 3;
-    this._secondCanvasContext.fillStyle = 'grey';
-    this._secondCanvasContext.beginPath();
-    this._secondCanvasContext.arc(80.64, 80.64, 80.64, Math.PI * 1.5, Math.PI * percent, false);
-    this._secondCanvasContext.arc(80.64, 80.64, 80.64 / 1.3, Math.PI * percent, Math.PI * 1.5, true);
-    this._secondCanvasContext.fill();
+    this._parsedFormatArr.map(format => {
+      const timeMeasure = this._timeMeasure[format];
+      const percent = 2 * parseInt(this.state[format], 10) / timeMeasure - 4.5;
+      const propsName = '_' + format + 'CanvasContext';
+      this[propsName].globalAlpha = 1 / 3;
+      this[propsName].fillStyle = 'grey';
+      this[propsName].fillText(this.state[format], this._radius, this._radius - 15);
+      this[propsName].fillText(format, this._radius, this._radius + 15);
+      this[propsName].beginPath();
+      this[propsName].arc(this._radius, this._radius, this._radius, Math.PI * 1.5, Math.PI * percent, false);
+      this[propsName].arc(this._radius, this._radius, this._radius / 1.3, Math.PI * percent, Math.PI * 1.5, true);
+      this[propsName].fill();
+    });
   }
 
   render() {
+    const format = (this._parseFormat(this._format));
     return (
       <div className="root-container">
         <section
           id="countdown"
           ref="countdown">
+            {format.day ? (
             <div className="years-container">
-              <span className="years" ref="years">{this.state.years}</span>
-              <div>Years</div>
-            </div>
+              <canvas
+                className="react-countdown-clock"
+                ref="year_canvas"
+                width={this.props.size}
+                height={this.props.size}>
+              </canvas>
+            </div> ) : null}
+            {format.day ? (
             <div>
-              <span className="days" ref="days">{this.state.days}</span>
-              <div>Days</div>
-            </div>
+              <canvas
+                className="react-countdown-clock"
+                ref="day_canvas"
+                width={this.props.size}
+                height={this.props.size}>
+              </canvas>
+            </div> ) : null}
+            {format.hour ? (
             <div>
-              <span className="hours" ref="hours">{this.state.hours}</span>
-              <div>Hours</div>
-            </div>
+              <canvas
+                className="react-countdown-clock"
+                ref="hour_canvas"
+                width={this.props.size}
+                height={this.props.size}>
+              </canvas>
+            </div> ) : null}
+            {format.minute ? (
             <div>
-              <span className="minutes"  ref="minutes">{this.state.minutes}</span>
-              <div>Minutes</div>
-            </div>
+              <canvas
+                className="react-countdown-clock"
+                ref="minute_canvas"
+                width={this.props.size}
+                height={this.props.size}>
+              </canvas>
+            </div> ) : null}
+            {format.second ? (
             <div>
-              <span className="seconds"  ref="seconds">{this.state.seconds}</span>
               <canvas
                 className="react-countdown-clock"
                 ref="second_canvas"
                 width={this.props.size}
                 height={this.props.size}>
               </canvas>
-              <div>Seconds</div>
-            </div>
+            </div> ) : null}
           </section>
       </div>
     );
