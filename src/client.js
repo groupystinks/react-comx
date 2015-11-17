9af2c56c9657b9afd208f2b6f2504f10f7e42078
Groupystinks <jason0911520@gmail.com>
2015-11-18 02:16:57 +0800

    modified route rule

--- src/client.js
+++ src/client.js
@@ -3,10 +3,14 @@ import {Router, Route, IndexRoute} from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Home from './Home';
-import createBrowserHistory from 'history/lib/createBrowserHistory';
+import { createHistory, useBasename } from 'history';
+
+const history = useBasename(createHistory)({
+  basename: '/react-comx',
+});
 
 const Routes = (
-  <Router history={createBrowserHistory()}>
+  <Router history={history}>
     <Route path="/" component={App}>
       <IndexRoute component={Home} />
       <Route path="/home" component={Home} />
