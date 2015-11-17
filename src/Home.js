dd3fce2761996b15fb06cc2d3c3c4d0456c86615
Groupystinks <jason0911520@gmail.com>
2015-11-18 01:40:56 +0800

    add home

---
+++ src/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+class Home extends Component {
+  render() {
+    return (
+      <section className="home-page">
+        <div>
+          Home
+        </div>
+      </section>
+    );
+  }
+}
+export default Home;
--- src/client.js
+++ src/client.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {Router, Route, IndexRoute} from 'react-router';
 import ReactDOM from 'react-dom';
 import App from './App';
+import Home from './Home';
 import createBrowserHistory from 'history/lib/createBrowserHistory';
 
 const Routes = (
   <Router history={createBrowserHistory()}>
     <Route path="/" component={App}>
-      <IndexRoute component={App} />
-      <Route path="/" component={App} />
+      <IndexRoute component={Home} />
+      <Route path="/home" component={Home} />
     </Route>
   </Router>
 );
